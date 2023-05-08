f635de33846b30b09bc1c5833588d250e8454adc
Ang Young <angieyou3@gmail.com>
2023-05-08 10:58:11 -0400

    adding buttons for future use of adding food and macros

--- src/services/FoodListService.js
+++ src/services/FoodListService.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-
-
 export default {
     //service call
     getAllFood() {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,10 @@
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
     <FoodList></FoodList>
+    <button @click="addFood">Add Food</button>
+    <button @click="setYourMacros">Set Your Macros</button>
   </div>
+  
 </template>
 
 <script>
