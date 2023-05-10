ce25c10c1140a65645252654b753158c45bf3ac2
Ang Young <angieyou3@gmail.com>
2023-05-10 10:07:17 -0400

    fixing display on home page to display users set macros

--- src/components/DailyMacroGoals.vue
+++ src/components/DailyMacroGoals.vue
@@ -1,11 +1,34 @@
 <template>
-
+<div>
+          <table>
+              <thead>
+                  <tr>
+                    
+                      <th>Protein</th>
+                      <th>Carbs</th>
+                      <th>Fats</th>
+                      <th>Calories</th>
+                    
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr v-for="item in foods" v-bind:key="item.macrosGoalId">
+                    
+                      <td>{{ item.protein }}</td>
+                      <td>{{ item.carbs }}</td>
+                      <td>{{ item.fats }}</td>
+                      <td>{{ item.calories }}</td>
+                     
+                  </tr>
+              </tbody>
+          </table>
+      </div>
        
   </template>
   
   <script>
   
- 
+  import foodListService from '../services/FoodListService.js'
   
   export default {
       name: 'food-list',
@@ -15,7 +38,7 @@
           }
       },
       created() {
-          foodListService.getAllFood().then(response => {
+          foodListService.getMacros().then(response => {
               this.foods = response.data;
               
           })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,25 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
-    <FoodList></FoodList>
-    <button @click="addFood">Add Food</button>
-    <button @click="setYourMacros">Set Your Macros</button>
+    <!-- <FoodList></FoodList> -->
+    <DailyMacroGoals></DailyMacroGoals>
+    <!-- <button @click="addFood">Add Food</button>
+    <button @click="setYourMacros">Set Your Macros</button> -->
   </div>
   
 </template>
 
 <script>
 
-import FoodList from '../components/FoodList.vue'
+// import FoodList from '../components/FoodList.vue'
+import DailyMacroGoals from '../components/DailyMacroGoals.vue';
 
 export default {
   name: "home-page",
 
   components: {
-    FoodList
+
+    DailyMacroGoals
   }
 };
 </script>
