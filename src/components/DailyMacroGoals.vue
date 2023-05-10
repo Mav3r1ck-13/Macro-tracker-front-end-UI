89c763914348c42c0c2bc566970cda07dab4fe32
Ang Young <angieyou3@gmail.com>
2023-05-10 14:46:56 -0400

    css formating

--- src/components/DailyMacroGoals.vue
+++ src/components/DailyMacroGoals.vue
@@ -1,51 +1,77 @@
 <template>
-<div>
-          <table>
-              <thead>
-                  <tr>
-                    
-                      <th>Protein</th>
-                      <th>Carbs</th>
-                      <th>Fats</th>
-                      <th>Calories</th>
-                    
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr v-for="item in foods" v-bind:key="item.macrosGoalId">
-                    
-                      <td>{{ item.protein }}</td>
-                      <td>{{ item.carbs }}</td>
-                      <td>{{ item.fats }}</td>
-                      <td>{{ item.calories }}</td>
-                     
-                  </tr>
-              </tbody>
-          </table>
-      </div>
-       
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th class="box">Protein</th>
+            <th class="box">Carbs</th>
+            <th class="box">Fats</th>
+            <th class="box">Calories</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in foods" v-bind:key="item.macrosGoalId">
+            <td class="box">{{ item.protein }}</td>
+            <td class="box">{{ item.carbs }}</td>
+            <td class="box">{{ item.fats }}</td>
+            <td class="box">{{ item.calories }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </template>
   
-  <script>
   
+  <script>
   import foodListService from '../services/FoodListService.js'
   
   export default {
-      name: 'food-list',
-      data() {
-          return {
-              foods: [],
-          }
-      },
-      created() {
-          foodListService.getMacros().then(response => {
-              this.foods = response.data;
-              
-          })
+    name: 'food-list',
+    data() {
+      return {
+        foods: [],
       }
+    },
+    created() {
+      foodListService.getMacros().then(response => {
+        this.foods = response.data;
+      })
+    }
   }
   </script>
   
   <style>
-  
+
+.box {
+  border: 1px solid #ccc;
+  padding: 5px;
+  text-align: center;
+  font-size: 15px;
+  border-radius: 5px;
+}
+
+th {
+  background-color: #24acb8;
+  font-size: 10px;
+  font-weight: bold;
+  border-radius: 5px;
+}
+
+thead th:first-child {
+  border-top-left-radius: 5px;
+}
+
+thead th:last-child {
+  border-top-right-radius: 5px;
+}
+
+tbody tr:last-child td:first-child {
+  border-bottom-left-radius: 5px;
+}
+
+tbody tr:last-child td:last-child {
+  border-bottom-right-radius: 5px;
+}
+
+
   </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
-    <!-- <FoodList></FoodList> -->
+    
     <DailyMacroGoals></DailyMacroGoals>
-    <!-- <button @click="addFood">Add Food</button>
-    <button @click="setYourMacros">Set Your Macros</button> -->
+    <button @click="addFood">Add Food</button>
+    <button @click="setYourMacros">Set Your Macros</button>
   </div>
   
 </template>
 
 <script>
 
-// import FoodList from '../components/FoodList.vue'
+
 import DailyMacroGoals from '../components/DailyMacroGoals.vue';
 
 export default {
